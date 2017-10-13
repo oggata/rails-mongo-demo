6ea4357575e168f918ef108adf0b054159da73e9
ogata-fumitoshi <ogata.fumitoshi@dena.jp>
2017-10-13 16:28:36 +0900

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   resources :comments
   resources :articles
   resources :cats
-
-
+  root to: 'articles#index'
   #get "/jp/articles/:id"           => "articles#show",       as: :article_id
   #get "/en/articles/:id"           => "articles#show",       as: :article_id
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
